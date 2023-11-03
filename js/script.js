46fc07d5777ff84489cd6f6ab7048339c79c7674
Lahlou ibrahim <ibrahimlahlou021@gmail.com>
2023-11-03 10:26:06 +0100

    First step refreshing page

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
 function refreshPage() {
-    // Replace all body content with the loading-gif
     document.body.innerHTML = `
         <div class="loading-gif">
             <img src="images/loading.gif" alt="Loading..." width="100">
         </div>
     `;
-
-    // After 5 seconds, refresh the page
+ 
     setTimeout(function() {
         location.reload();
-    }, 5000); // 5 seconds delay
+    }, 5000); 
 }
 
-// Refresh the page every 20 seconds
 setInterval(function() {
     refreshPage();
-}, 20000); //
+}, 20000); 
